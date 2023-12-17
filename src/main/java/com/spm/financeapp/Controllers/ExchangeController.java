d1bd0d2b98167f4420ab94a96375abd18cddc989
Ömer Faruk Aydın <o.faduzce@gmail.com>
2023-12-17 21:10:08 +0300

    Exchange search option added

--- src/main/java/com/spm/financeapp/Controllers/ExchangeController.java
+++ src/main/java/com/spm/financeapp/Controllers/ExchangeController.java
@@ -8,6 +8,8 @@ import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -21,7 +23,7 @@ public class ExchangeController {
 
     @GetMapping("/exchange")
     public String getExchange(Model model) throws IOException {
-            URL url = new URL("https://api.freecurrencyapi.com/v1/latest?apikey=DllhFwwu1qAlOhyuP2EpcANEnekNYBhIDhjR5uRu");
+        URL url = new URL("https://api.freecurrencyapi.com/v1/latest?apikey=DllhFwwu1qAlOhyuP2EpcANEnekNYBhIDhjR5uRu");
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         conn.connect();
@@ -57,5 +59,48 @@ public class ExchangeController {
         //-----
         return "exchange";
 
+    }
+    @PostMapping("/exchange")
+    public String postExchange(@RequestParam(value = "currency", required = false) String currency, Model model) throws IOException {
+        if (currency != null && (currency.length() == 3)) {
+            URL url = new URL("https://api.freecurrencyapi.com/v1/latest?apikey=DllhFwwu1qAlOhyuP2EpcANEnekNYBhIDhjR5uRu&currencies=" + currency.toUpperCase());
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            String inline = "";
+            int responceCode = conn.getResponseCode();
+            if(responceCode != 200){
+                throw new RuntimeException("HTTP Response Code: "+responceCode);
+
+            }
+            else{
+
+                Scanner scn = new Scanner(url.openStream());
+                while (scn.hasNext()){
+                    inline += scn.nextLine();
+                }
+                scn.close();
+            }
+            JSONObject obj = new JSONObject(inline);
+            //-------
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                ExchangeRatesResponse exchangeRatesResponse = objectMapper.readValue(inline, ExchangeRatesResponse.class);
+
+                List<Exchange> exchangeList = new ArrayList<>();
+                exchangeRatesResponse.getData().forEach((name, value) -> {
+                    exchangeList.add(new Exchange(name, value));
+                });
+
+                model.addAttribute("exchanges", exchangeList);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //-----
+            return "exchange";
+        }
+
+        return "exchange";
     }
 }
--- src/main/java/com/spm/financeapp/FinanceAppApplication.java
+++ src/main/java/com/spm/financeapp/FinanceAppApplication.java
@@ -3,15 +3,22 @@ package com.spm.financeapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 //@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 //@ServletComponentScan
-public class FinanceAppApplication {
+public class FinanceAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinanceAppApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(FinanceAppApplication.class);
 	}
 
 }
