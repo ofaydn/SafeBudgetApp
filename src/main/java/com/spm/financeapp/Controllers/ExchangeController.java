5947e85c6b58ba45cef82cdad7a37242c5e27f34
Ömer Faruk Aydın <o.faduzce@gmail.com>
2023-12-17 22:12:28 +0300

    Error detection
    Error detection for wrong currency input

--- src/main/java/com/spm/financeapp/Controllers/ExchangeController.java
+++ src/main/java/com/spm/financeapp/Controllers/ExchangeController.java
@@ -12,18 +12,52 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 @Controller
 public class ExchangeController {
+    String apiURL = "https://api.freecurrencyapi.com/v1/latest?apikey=DllhFwwu1qAlOhyuP2EpcANEnekNYBhIDhjR5uRu";
+
 
     @GetMapping("/exchange")
     public String getExchange(Model model) throws IOException {
-        URL url = new URL("https://api.freecurrencyapi.com/v1/latest?apikey=DllhFwwu1qAlOhyuP2EpcANEnekNYBhIDhjR5uRu");
+        ArrayList<Exchange> exchangeList = new ArrayList<Exchange>();
+        exchangeList = getList(exchangeList,apiURL);
+        model.addAttribute("exchanges", exchangeList);
+        return "exchange";
+
+    }
+    @PostMapping("/exchange")
+    public String postExchange(@RequestParam(value = "currency", required = false) String currency, Model model) throws IOException {
+        ArrayList<String> currArr = new ArrayList<String>();
+        ArrayList<Exchange> exList = new ArrayList<Exchange>();
+        Collections.addAll(currArr,"USD","AUD","BGN","BRL","CAD","CHF","CNY","CZK","DKK","EUR","GBP","HKD","HRK","HUF","IDR","ILS","INR","ISK","JPY",
+                "KRW","MXN","MYR","NOK","NZD","PHP","PLN","RON","RUB","SEK","SGD","THB","TRY","ZAR");
+
+        if (currArr.contains(currency.toUpperCase())) {
+            System.out.println("dogru");
+            String url = apiURL + "&currencies=" + currency.toUpperCase();
+            exList = getList(exList,url);
+            model.addAttribute("exchanges",exList);
+            return "exchange";
+        }else{
+            System.out.println("yanlis");
+
+            exList = getList(exList,apiURL);
+            model.addAttribute("exchanges",exList);
+            return "exchange";
+        }
+
+
+
+
+
+    }
+    public ArrayList<Exchange> getList(ArrayList<Exchange> arrList, String url1) throws IOException {
+        URL url = new URL(url1);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         conn.connect();
@@ -41,66 +75,15 @@ public class ExchangeController {
             scn.close();
         }
         JSONObject obj = new JSONObject(inline);
-        //-------
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             ExchangeRatesResponse exchangeRatesResponse = objectMapper.readValue(inline, ExchangeRatesResponse.class);
-
-            List<Exchange> exchangeList = new ArrayList<>();
             exchangeRatesResponse.getData().forEach((name, value) -> {
-                exchangeList.add(new Exchange(name, value));
+                arrList.add(new Exchange(name, value));
             });
-
-            model.addAttribute("exchanges", exchangeList);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        //-----
-        return "exchange";
-
-    }
-    @PostMapping("/exchange")
-    public String postExchange(@RequestParam(value = "currency", required = false) String currency, Model model) throws IOException {
-        if (currency != null && (currency.length() == 3)) {
-            URL url = new URL("https://api.freecurrencyapi.com/v1/latest?apikey=DllhFwwu1qAlOhyuP2EpcANEnekNYBhIDhjR5uRu&currencies=" + currency.toUpperCase());
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.connect();
-            String inline = "";
-            int responceCode = conn.getResponseCode();
-            if(responceCode != 200){
-                throw new RuntimeException("HTTP Response Code: "+responceCode);
-
-            }
-            else{
-
-                Scanner scn = new Scanner(url.openStream());
-                while (scn.hasNext()){
-                    inline += scn.nextLine();
-                }
-                scn.close();
-            }
-            JSONObject obj = new JSONObject(inline);
-            //-------
-            ObjectMapper objectMapper = new ObjectMapper();
-            try {
-                ExchangeRatesResponse exchangeRatesResponse = objectMapper.readValue(inline, ExchangeRatesResponse.class);
-
-                List<Exchange> exchangeList = new ArrayList<>();
-                exchangeRatesResponse.getData().forEach((name, value) -> {
-                    exchangeList.add(new Exchange(name, value));
-                });
-
-                model.addAttribute("exchanges", exchangeList);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            //-----
-            return "exchange";
-        }
-
-        return "exchange";
+        return arrList;
     }
 }
